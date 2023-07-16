657ef75312b50e467cc2dc4c743d3e922782ab1b
최은석 <trgf456@naver.com>
2023-07-16 13:42:35 +0900

    404 not found page

---
+++ src/routes/404.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const ErrorPageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #09171c;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  h1 {
+    font-size: 4rem;
+  }
+  h3 {
+    font-size: 2rem;
+  }
+`;
+
+const ErrorPage = () => {
+  return (
+    <ErrorPageContainer>
+      <div>
+        <h1>404</h1>
+        <h3>not found</h3>
+      </div>
+    </ErrorPageContainer>
+  );
+};
+export default ErrorPage;
